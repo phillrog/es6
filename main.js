b67da89c2ea961a0f0858bebd97433f8cf2ee513
fh.proger <phillipe.souza@rte.com.br>
2020-04-06 21:59:42 -0300

    Classes

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-alert('Teste');
+class List {
+    constructor() {
+        this.data = [];
+    }
 
-class Teste {
-    Cacular() {}
-
-    Calcular2() {}
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
 }
+
+class TodoList extends List{
+    constructor(){
+        super();
+
+        this.usuario = 'Phillipe';
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario);
+    }   
+}
+
+class Matematica {
+    static soma(a,b) {
+        return a+b;
+    }
+}
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.add('Novo Todo');
+}
+
+MinhaLista.mostraUsuario();
+
+console.log(Matematica.soma(10,2));
