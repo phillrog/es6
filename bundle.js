8e592a2fae1a8ec73fa314996bc05aca8e0b269b
fh.proger <phillipe.souza@rte.com.br>
2020-04-06 22:11:09 -0300

    Vetores

--- bundle.js
+++ bundle.js
@@ -103,4 +103,22 @@ function teste(z) {
   }
 }
 
-teste(10);
+teste(10); //vetor
+
+var arr = [1, 2, 3, 4, 5, 6];
+var newArr = arr.map(function (item, index) {
+  return item + index;
+});
+console.log(newArr);
+var sum = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(sum);
+var filter = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filter);
+var find = arr.find(function (item) {
+  return item == 6;
+});
+console.log(find);
--- main.js
+++ main.js
@@ -53,3 +53,31 @@ function teste(z) {
 }
 
 teste(10);
+
+//vetor
+
+const arr = [1,2,3,4,5,6];
+
+const newArr = arr.map(function(item, index) {
+    return item + index;
+});
+
+console.log(newArr);
+
+const sum = arr.reduce(function(total, next) {
+    return total + next;
+});
+
+console.log(sum);
+
+const filter = arr.filter(function(item) {
+    return item % 2 === 0;
+});
+
+console.log(filter);
+
+const find = arr.find(function(item) {
+    return item == 6;
+});
+
+console.log(find);
