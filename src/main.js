58a688259d7271d757bf6d070fd1fc796d08c365
fh.proger <phillipe.souza@rte.com.br>
2020-04-07 21:56:01 -0300

    Buscando da api

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import api from './api';
+
 class App {
     constructor () {
         this.respositories = [];
 
         this.formEl = document.getElementById('repo-form');
         this.listEl = document.getElementById('repo-list');
+        this.inputEl = document.querySelector('input[name=repository]');
 
         this.registerHandlers();
     }
@@ -12,16 +15,28 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event) {
+    async addRepository(event) {
         event.preventDefault();
 
+        const repoInput = this.inputEl.value;
+
+        if (repoInput.length === 0  )
+            return;
+
+        const response = await api.get(`repos/${repoInput}`);
+
+        const {name, description, html_url, owner: { avatar_url }} = response.data;
+        
+
         this.respositories.push({
-            name: 'rocketseat.com.br',
-            description: 'Tire a sua idéia do papel e dê vida à sua startup',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v4',
-            html_url: 'https://github.com/phillrog/es6/tree/4-AplicacaoComEs6'
+            name,
+            description,
+            avatar_url,
+            html_url
         });
 
+        this.inputEl.value = '';
+        
         this.render();
     }
 
